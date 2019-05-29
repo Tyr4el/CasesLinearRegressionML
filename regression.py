bb5da772b0e81bb5c50bb7788ec85762aa37457d
Nick Conklin <nconkl1@outlook.com>
2019-05-29 14:06:33 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -6,11 +6,11 @@ from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('Opened_Cases_RR_AllTime_Modified.csv', usecols=['Days Since Start Date', 'Day of Week', '# of Cases'])
 
-scaler_x = MinMaxScaler()
-scaler_y = MinMaxScaler()
-
 x = df.iloc[:, :-1].values
 y = df.iloc[:, 2].values.reshape(-1, 1)
+
+scaler_x = MinMaxScaler()
+scaler_y = MinMaxScaler()
 
 x_scaled = scaler_x.fit_transform(x)
 y_scaled = scaler_y.fit_transform(y)
