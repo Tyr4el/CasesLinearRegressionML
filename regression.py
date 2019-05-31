9146c66160ee47c698fc4601b542fbe7ca1dae5a
Nick Conklin <nconkl1@outlook.com>
2019-05-31 14:09:24 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -25,6 +25,7 @@ x_scaled = scaler_x.fit_transform(x)
 y_scaled = scaler_y.fit_transform(y)
 
 # Train Test Split that shit
+# Pass in x and y if we want to train on the non-scaled values
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=0.2, random_state=42)
 
 # Create our Linear Regression object
@@ -33,9 +34,21 @@ regressor = LinearRegression()
 # Fit the Linear Regression object to our training data
 regressor.fit(x_train, y_train)
 
+# Predict the test set results
+y_pred = regressor.predict(x_test)
+
+# Visualize the training set results
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+
+
+plt.show()
+
+
+
 # Debugging shit
 print(len(x_train))
-print(len(x_test))
+print(len(y_train))
 # print(x_scaled)
 # print("--------------------------------------")
 # print(y_scaled)
