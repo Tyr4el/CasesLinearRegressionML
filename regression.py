ad22823cbce97f42244def5b7c1aaf69697f92a5
Nick Conklin <nconkl1@outlook.com>
2019-05-31 13:53:59 -0400

    Added LinearRegression

--- regression.py
+++ regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Read the CSV
 df = pd.read_csv('Opened_Cases_RR_AllTime_Modified.csv', usecols=['Days Since Start Date', 'Day of Week', '# of Cases'])
@@ -26,7 +27,15 @@ y_scaled = scaler_y.fit_transform(y)
 # Train Test Split that shit
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=0.2, random_state=42)
 
+# Create our Linear Regression object
+regressor = LinearRegression()
+
+# Fit the Linear Regression object to our training data
+regressor.fit(x_train, y_train)
+
 # Debugging shit
-print(x_scaled)
-print("--------------------------------------")
-print(y_scaled)
+print(len(x_train))
+print(len(x_test))
+# print(x_scaled)
+# print("--------------------------------------")
+# print(y_scaled)
